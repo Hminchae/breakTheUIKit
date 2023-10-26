5260b9076f6ff7a06ac4b7bd5fe595cafc34bf22
Hminchae <hminchae@gmail.com>
2023-10-27 04:22:24 +0900

    update MusicPlayer

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 황민채 on 10/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
